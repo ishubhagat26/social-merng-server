c8a72a7b3d2b2648031b8e1c162721d46d3b0432
Ishu Bhagat <ishu.bhagat26@gmail.com>
2021-07-08 04:57:32 +0530

    included config

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MONGODB: 'mongodb+srv://bhagat26:hDETJb54hVRZgF8@cluster0.hfflc.mongodb.net/merng?retryWrites=true&w=majority',
+    SECRET_KEY: 'Some very secret key'
+    
+}
