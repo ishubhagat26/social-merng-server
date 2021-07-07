e36ca2b09e19c1d12c403fab06d05ac1b97de39f
Ishu Bhagat <ishu.bhagat26@gmail.com>
2021-07-08 05:05:08 +0530

    2nd

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-  
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer, PubSub } = require('apollo-server');
 const mongoose = require('mongoose');
 
 const typeDefs = require('./graphql/typeDefs');
@@ -8,12 +7,12 @@ const { MONGODB } = require('./config.js');
 
 const pubsub = new PubSub();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.port || 5000;
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: ({ req }) => ({ req })
+  context: ({ req }) => ({ req, pubsub })
 });
 
 mongoose
@@ -26,5 +25,5 @@ mongoose
     console.log(`Server running at ${res.url}`);
   })
   .catch(err => {
-    console.log(err);
+    console.error(err)
   })
